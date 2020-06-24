ebbdffc8590736aa5bec8b67c336a3993b1da216
ANTHONY CLEMENTE OLIVA MURILLO <anthony.oliva80@unitec.edu>
2020-06-24 01:04:14 -0600

    av1

--- Principal_Internet_elvis/Principal_Internet_elvis/Usuarios.Designer.cs
+++ Principal_Internet_elvis/Principal_Internet_elvis/Usuarios.Designer.cs
@@ -38,13 +38,13 @@
             this.txt_descp = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.bt_crear = new System.Windows.Forms.Button();
-            this.bt_modificar = new System.Windows.Forms.Button();
-            this.bt_eliminar = new System.Windows.Forms.Button();
             this.bt_limpiar = new System.Windows.Forms.Button();
+            this.bt_eliminar = new System.Windows.Forms.Button();
+            this.bt_modificar = new System.Windows.Forms.Button();
+            this.bt_crear = new System.Windows.Forms.Button();
             this.groupBox6 = new System.Windows.Forms.GroupBox();
-            this.txt_roles = new System.Windows.Forms.TextBox();
             this.bt_elegir_roles = new System.Windows.Forms.Button();
+            this.txt_roles = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -56,13 +56,14 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
             this.groupBox1.Controls.Add(this.txt_id);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(113, 49);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "ID Usuario";
+            this.groupBox1.Text = "CODIGO";
             // 
             // txt_id
             // 
@@ -73,13 +74,14 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.BackColor = System.Drawing.Color.White;
             this.groupBox2.Controls.Add(this.txt_usuario);
             this.groupBox2.Location = new System.Drawing.Point(12, 67);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(176, 51);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Usuario";
+            this.groupBox2.Text = "USUARIO";
             // 
             // txt_usuario
             // 
@@ -90,13 +92,14 @@
             // 
             // groupBox3
             // 
+            this.groupBox3.BackColor = System.Drawing.Color.White;
             this.groupBox3.Controls.Add(this.txt_clave);
             this.groupBox3.Location = new System.Drawing.Point(12, 124);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(176, 51);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Clave";
+            this.groupBox3.Text = "NOMBRE";
             // 
             // txt_clave
             // 
@@ -107,13 +110,14 @@
             // 
             // groupBox4
             // 
+            this.groupBox4.BackColor = System.Drawing.Color.White;
             this.groupBox4.Controls.Add(this.txt_descp);
             this.groupBox4.Location = new System.Drawing.Point(12, 181);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(176, 51);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Descripcion";
+            this.groupBox4.Text = "DESCRIPCION";
             // 
             // txt_descp
             // 
@@ -127,71 +131,90 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(194, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(238, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(370, 202);
+            this.dataGridView1.Size = new System.Drawing.Size(405, 221);
             this.dataGridView1.TabIndex = 4;
             // 
             // groupBox5
             // 
+            this.groupBox5.BackColor = System.Drawing.Color.White;
             this.groupBox5.Controls.Add(this.bt_limpiar);
             this.groupBox5.Controls.Add(this.bt_eliminar);
             this.groupBox5.Controls.Add(this.bt_modificar);
             this.groupBox5.Controls.Add(this.bt_crear);
-            this.groupBox5.Location = new System.Drawing.Point(194, 220);
+            this.groupBox5.Location = new System.Drawing.Point(238, 243);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(370, 57);
+            this.groupBox5.Size = new System.Drawing.Size(405, 69);
             this.groupBox5.TabIndex = 5;
             this.groupBox5.TabStop = false;
-            this.groupBox5.Text = "Opciones";
-            // 
-            // bt_crear
-            // 
-            this.bt_crear.Location = new System.Drawing.Point(6, 19);
-            this.bt_crear.Name = "bt_crear";
-            this.bt_crear.Size = new System.Drawing.Size(84, 38);
-            this.bt_crear.TabIndex = 0;
-            this.bt_crear.Text = "CREAR";
-            this.bt_crear.UseVisualStyleBackColor = true;
-            // 
-            // bt_modificar
-            // 
-            this.bt_modificar.Location = new System.Drawing.Point(96, 19);
-            this.bt_modificar.Name = "bt_modificar";
-            this.bt_modificar.Size = new System.Drawing.Size(84, 38);
-            this.bt_modificar.TabIndex = 1;
-            this.bt_modificar.Text = "MODIFICAR";
-            this.bt_modificar.UseVisualStyleBackColor = true;
-            // 
-            // bt_eliminar
-            // 
-            this.bt_eliminar.Location = new System.Drawing.Point(186, 19);
-            this.bt_eliminar.Name = "bt_eliminar";
-            this.bt_eliminar.Size = new System.Drawing.Size(84, 38);
-            this.bt_eliminar.TabIndex = 2;
-            this.bt_eliminar.Text = "ELIMINAR";
-            this.bt_eliminar.UseVisualStyleBackColor = true;
+            this.groupBox5.Text = "OPCIONES";
             // 
             // bt_limpiar
             // 
-            this.bt_limpiar.Location = new System.Drawing.Point(276, 19);
+            this.bt_limpiar.BackColor = System.Drawing.Color.White;
+            this.bt_limpiar.Location = new System.Drawing.Point(311, 19);
             this.bt_limpiar.Name = "bt_limpiar";
-            this.bt_limpiar.Size = new System.Drawing.Size(84, 38);
+            this.bt_limpiar.Size = new System.Drawing.Size(88, 38);
             this.bt_limpiar.TabIndex = 3;
             this.bt_limpiar.Text = "LIMPIAR";
-            this.bt_limpiar.UseVisualStyleBackColor = true;
+            this.bt_limpiar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bt_limpiar.UseVisualStyleBackColor = false;
+            // 
+            // bt_eliminar
+            // 
+            this.bt_eliminar.BackColor = System.Drawing.Color.White;
+            this.bt_eliminar.Location = new System.Drawing.Point(210, 19);
+            this.bt_eliminar.Name = "bt_eliminar";
+            this.bt_eliminar.Size = new System.Drawing.Size(95, 38);
+            this.bt_eliminar.TabIndex = 2;
+            this.bt_eliminar.Text = "ELIMINAR";
+            this.bt_eliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bt_eliminar.UseVisualStyleBackColor = false;
+            // 
+            // bt_modificar
+            // 
+            this.bt_modificar.BackColor = System.Drawing.Color.White;
+            this.bt_modificar.Location = new System.Drawing.Point(109, 19);
+            this.bt_modificar.Name = "bt_modificar";
+            this.bt_modificar.Size = new System.Drawing.Size(95, 38);
+            this.bt_modificar.TabIndex = 1;
+            this.bt_modificar.Text = "MODIFICAR";
+            this.bt_modificar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bt_modificar.UseVisualStyleBackColor = false;
+            // 
+            // bt_crear
+            // 
+            this.bt_crear.BackColor = System.Drawing.Color.White;
+            this.bt_crear.Location = new System.Drawing.Point(6, 19);
+            this.bt_crear.Name = "bt_crear";
+            this.bt_crear.Size = new System.Drawing.Size(97, 38);
+            this.bt_crear.TabIndex = 0;
+            this.bt_crear.Text = "CREAR";
+            this.bt_crear.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bt_crear.UseVisualStyleBackColor = false;
             // 
             // groupBox6
             // 
+            this.groupBox6.BackColor = System.Drawing.Color.White;
             this.groupBox6.Controls.Add(this.bt_elegir_roles);
             this.groupBox6.Controls.Add(this.txt_roles);
-            this.groupBox6.Location = new System.Drawing.Point(12, 238);
+            this.groupBox6.Location = new System.Drawing.Point(38, 243);
             this.groupBox6.Name = "groupBox6";
-            this.groupBox6.Size = new System.Drawing.Size(176, 51);
+            this.groupBox6.Size = new System.Drawing.Size(194, 69);
             this.groupBox6.TabIndex = 6;
             this.groupBox6.TabStop = false;
-            this.groupBox6.Text = "Roles";
+            this.groupBox6.Text = "ROLES";
+            // 
+            // bt_elegir_roles
+            // 
+            this.bt_elegir_roles.Location = new System.Drawing.Point(163, 27);
+            this.bt_elegir_roles.Name = "bt_elegir_roles";
+            this.bt_elegir_roles.Size = new System.Drawing.Size(25, 23);
+            this.bt_elegir_roles.TabIndex = 7;
+            this.bt_elegir_roles.Text = "...";
+            this.bt_elegir_roles.UseVisualStyleBackColor = true;
             // 
             // txt_roles
             // 
@@ -200,21 +223,12 @@
             this.txt_roles.Size = new System.Drawing.Size(133, 20);
             this.txt_roles.TabIndex = 2;
             // 
-            // bt_elegir_roles
-            // 
-            this.bt_elegir_roles.Location = new System.Drawing.Point(145, 18);
-            this.bt_elegir_roles.Name = "bt_elegir_roles";
-            this.bt_elegir_roles.Size = new System.Drawing.Size(25, 23);
-            this.bt_elegir_roles.TabIndex = 7;
-            this.bt_elegir_roles.Text = "...";
-            this.bt_elegir_roles.UseVisualStyleBackColor = true;
-            // 
             // Usuarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(577, 300);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(655, 324);
             this.Controls.Add(this.groupBox6);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.dataGridView1);
